529f4026f120f897d76c0c1c89e3ae4215d98057
zhengchangjun <zhengchangjun@jd..com>
2019-08-28 18:45:52 +0800

    a

--- app.ts
+++ app.ts
@@ -1,1 +1,18 @@
-let num = 4;
+// let num = 4;
+import * as WebSocket from 'ws';
+
+const host = 'wss://7cec1713-ws.va.huya.com';
+const ws = new WebSocket(host);
+
+ws.on('open', function open() {
+  console.log('open')
+  // ws.send('something');
+});
+
+ws.on('message', function incoming(data) {
+  console.log(data);
+});
+
+ws.on('close', function close() {
+  console.log('disconnected');
+});
