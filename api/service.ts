c08ad7efd52ac2725f5c8140bccc2caf988dd65a
zcj <1248198090@qq.com>
2019-09-10 23:53:36 +0800

    a

--- api/service.ts
+++ api/service.ts
@@ -4,3 +4,7 @@ import CONFIG from './../const/CONFIG';
 export function getWssHosts(headers) {
 	return post(CONFIG.wupHost, null, { headers });
 }
+
+export function passwordLogin(data) {
+	return post('https://udblgn.huya.com/web/v2/passwordLogin', data);
+}
