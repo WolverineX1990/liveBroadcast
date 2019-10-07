061bdf6e4cbfb594f1fb473bd12870323eebb75c
zhengchangjun <zhengchangjun@jd..com>
2019-10-07 22:54:54 +0800

    a

--- app.ts
+++ app.ts
@@ -3,12 +3,22 @@ import HuyaIns from './core/HuyaIns';
 global['storage'] = {};
 
 const roomId = 19622208;
+// const roomId = 19322980;
 let users = ['17188945517', '18162814162', '15687472565', '18257431411', '17105112341'];
-let demo = new HuyaIns(roomId, '17105112341', 'hy17105112341');
-demo.startConnect();
-// let arr = [];
-// for (let i = 0; i<5; i++) {
-//     let demo = new HuyaIns(roomId, users[i], 'hy'+users[i]);
-//     demo.startConnect();
-//     arr.push(demo);
-// }
+// let demo = new HuyaIns(roomId, '17105112341', 'hy17105112341');
+// demo.startConnect();
+let arr = [];
+function enterRoom() {
+  console.log('enterRoom');
+  let user = users.shift();
+  let demo = new HuyaIns(roomId, user, 'hy'+user);
+  demo.startConnect();
+  arr.push(demo);
+  if (users.length) {
+    let timeout = Math.round(Math.random()*5);
+    setTimeout(() => enterRoom(), timeout * 1000);
+  }
+}
+
+//开始
+enterRoom();
