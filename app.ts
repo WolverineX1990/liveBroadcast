e9b373f8e0ff1e6c574484419d1d25714da88733
zhengchangjun <zhengchangjun@jd..com>
2019-10-07 21:47:51 +0800

    a

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ global['storage'] = {};
 
 const roomId = 19622208;
 let users = ['17188945517', '18162814162', '15687472565', '18257431411', '17105112341'];
-let demo = new HuyaIns(roomId, '17188945517', 'hy17188945517');
+let demo = new HuyaIns(roomId, '17105112341', 'hy17105112341');
 demo.startConnect();
 // let arr = [];
 // for (let i = 0; i<5; i++) {
