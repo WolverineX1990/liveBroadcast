75258a06d5ac6bb6aa5a8713bc8ac563636efaf3
zhengchangjun <zhengchangjun@jd..com>
2019-10-07 20:54:10 +0800

    d

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import HuyaIns from './core/HuyaIns';
 global['storage'] = {};
 
 const roomId = 19622208;
-let users = ['16531454346', '16533154856', '17004489272', '17115432968', '17115439863'];
+let users = ['17188945517', '18162814162', '15687472565', '16564419417', '17105112341'];
 // let demo = new HuyaIns(roomId, '16533154856', 'hy16533154856');
 // demo.startConnect();
 let arr = [];
