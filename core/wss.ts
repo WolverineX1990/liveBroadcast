e191c115435e5b872ee0f93a3a84d0ca1e5b9567
zhengchangjun <zhengchangjun@jd..com>
2019-10-08 11:35:48 +0800

    a

--- core/wss.ts
+++ core/wss.ts
@@ -11,7 +11,7 @@ export default class Wss {
     start (openFun, messageFun, closeFun) {
         let ws = new WebSocket(this.host, {
             Origin: 'https://www.huya.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
+            'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'
         });
         this.ws = ws;
         ws.on('open', function open() {
